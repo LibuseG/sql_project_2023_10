57571fe7d1dfa187058b819bdda339ce00626af6
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-09 12:02:23 +0100

    initial commit

--- Projekt_1.sql
+++ Projekt_1.sql
@@ -1,1 +1,31 @@
 
+
+SELECT
+    name, value AS avg_payroll, payroll_year, payroll_quarter 
+FROM czechia_payroll_industry_branch cpib 
+LEFT JOIN
+czechia_payroll cp 
+ON cpib.code = cp.industry_branch_code
+WHERE value IS NOT NULL AND value_type_code = 5958
+ORDER BY name, payroll_year, payroll_quarter ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ctreate TABLE t_libuse_gregorova_project_SQL_primary_final as
