641a58b05afbaa3b837fd93e86b6b001e437cee8
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-10 20:02:07 +0100

    first and last period

--- Projekt_4.sql
+++ Projekt_4.sql
@@ -6,6 +6,10 @@ v dostupných datech cen a mezd?--
 SELECT*
 FROM czechia_price_category;
 
+SELECT DISTINCT
+        payroll_year
+        FROM t_libuse_gregorova_project_sql_primary_final tlgpspf 
+       group BY payroll_year;
 
 WITH milk_and_bread AS (
     SELECT category_code
@@ -13,9 +17,10 @@ WITH milk_and_bread AS (
     WHERE category_code = 111301 OR category_code = 114201
 )
 SELECT a.food_category , a.payroll_year, 
-	round (a.avg_payroll/a.price) AS amount
+	round(a.avg_payroll/a.price) AS amount, a.price_unit
 FROM milk_and_bread 
-RIGHT JOIN t_libuse_gregorova_project_sql_primary_final AS a
+left JOIN t_libuse_gregorova_project_sql_primary_final AS a
 ON milk_and_bread.category_code = a.category_code 
+ AND a.payroll_year IN (2006, 2018)
 group BY a.payroll_year
 ;
