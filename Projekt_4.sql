b0cd27e67f9f4bd1f9517ce3dbe0358e21a13562
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-11 19:19:19 +0100

    add grouping

--- Projekt_4.sql
+++ Projekt_4.sql
@@ -16,11 +16,11 @@ WITH milk_and_bread AS (
     FROM czechia_price
     WHERE category_code = 111301 OR category_code = 114201
 )
-SELECT a.food_category , a.payroll_year, 
+SELECT a.food_category , a.payroll_year, avg(a.avg_payroll), avg(a.price),
 	round(a.avg_payroll/a.price) AS amount, a.price_unit
 FROM milk_and_bread 
 left JOIN t_libuse_gregorova_project_sql_primary_final AS a
 ON milk_and_bread.category_code = a.category_code 
  AND a.payroll_year IN (2006, 2018)
-group BY a.payroll_year
+group BY a.payroll_year, a.food_category
 ;
