839f2131dbc9c830c58e78eb66d4ec1da5158d6a
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-13 14:54:35 +0100

    adding first year and last year values

--- Projekt_3.sql
+++ Projekt_3.sql
@@ -2,9 +2,33 @@
 
 -- 1.	Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?--
 
-SELECT name,
-	min (payroll_year) AS first_year,
-	max (payroll_year) AS last_year
+
+SELECT
+    a.name,
+    first_year_payroll,
+    last_year_payroll
+FROM (
+	SELECT
+	name,
+    round(avg (avg_payroll)) AS last_year_payroll
+	FROM t_libuse_gregorova_project_SQL_primary_final
+	WHERE payroll_year  IN (
+    	SELECT
+        MAX(payroll_year)
+    	FROM t_libuse_gregorova_project_SQL_primary_final)
+    	GROUP BY name
+    ) a
+LEFT JOIN (
+SELECT
+	name,
+    round(avg (avg_payroll)) AS first_year_payroll
 FROM t_libuse_gregorova_project_SQL_primary_final
-GROUP BY name ;
-	
+WHERE payroll_year  IN (
+    SELECT
+        min(payroll_year)
+    FROM t_libuse_gregorova_project_SQL_primary_final)
+    GROUP BY name
+    ) b
+ON a.name = b.name
+;
+
