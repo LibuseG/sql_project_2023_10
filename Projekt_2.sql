238271e5b43d719adbcc344979d1c0b89df1ff70
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-13 17:25:59 +0100

    additional colums with payroll and price

--- Projekt_2.sql
+++ Projekt_2.sql
@@ -1,8 +1,17 @@
 
-CREATE OR REPLACE TABLE t_{jmeno}_{prijmeni}_project_SQL_secondary_final AS
-SELECT e.YEAR, GDP AS GDP_Czech_Republic
+CREATE OR REPLACE TABLE t_libuse_gregorova_project_SQL_secondary_final AS
+SELECT e.YEAR, cp.value AS payroll, cp2.value AS price, GDP AS GDP_Czech_Republic
 FROM economies e 
 JOIN countries c 
     ON e.country = c.country 
 	AND e.country = 'Czech Republic'    	
-	AND GDP IS NOT NULL;
+	AND GDP IS NOT NULL
+LEFT JOIN 
+	czechia_payroll cp 
+ON e.YEAR = cp.payroll_year AND
+    cp.value_type_code = 5958 AND 
+    calculation_code = 200   
+JOIN czechia_price cp2 
+ON e.YEAR = YEAR(cp2.date_from) AND 
+	cp2.region_code IS NULL
+GROUP BY YEAR 	;
