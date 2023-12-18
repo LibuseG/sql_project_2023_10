a456ce837b4cbad89e5c0db1a623d487eeeff3b8
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-18 19:27:10 +0100

    adding gini, population and other european countries

--- Projekt_2.sql
+++ Projekt_2.sql
@@ -1,17 +1,23 @@
 
+
+CREATE OR REPLACE TEMPORARY TABLE europe_countries AS (
+	SELECT country, year, population, GDP, gini
+	FROM economies 
+	WHERE GDP IS NOT NULL AND gini IS NOT NULL
+);
 CREATE OR REPLACE TABLE t_libuse_gregorova_project_SQL_secondary_final AS
-SELECT e.YEAR, cp.value AS payroll, avg(cp2.value) AS price, GDP AS GDP_Czech_Republic
-FROM economies e 
+SELECT ec.country, ec.YEAR, ec.population, ec.GDP, ec.gini,
+		cp.value AS payroll, avg(cp2.value) AS price
+FROM europe_countries ec 
 JOIN countries c 
-    ON e.country = c.country 
-	AND e.country = 'Czech Republic'    	
-	AND GDP IS NOT NULL
-LEFT JOIN 
+    ON ec.country = c.country
+    AND region_in_world LIKE '%Europe%'
+JOIN 
 	czechia_payroll cp 
-ON e.YEAR = cp.payroll_year AND
+ON ec.YEAR = cp.payroll_year AND
     cp.value_type_code = 5958 AND 
     calculation_code = 200   
 JOIN czechia_price cp2 
-ON e.YEAR = YEAR(cp2.date_from) AND 
+ON ec.YEAR = YEAR(cp2.date_from) AND 
 	cp2.region_code IS NULL 
-GROUP BY year;
+;
