0cfca25bcc1c519aacd773e4312001ee5e3e85e0
Libuše Gregorová <libusegregorova@seznam.cz>
2023-12-19 20:59:26 +0100

    final commit

--- Projekt_2.sql
+++ Projekt_2.sql
@@ -6,18 +6,18 @@ CREATE OR REPLACE TEMPORARY TABLE europe_countries AS (
 	WHERE GDP IS NOT NULL AND gini IS NOT NULL
 );
 CREATE OR REPLACE TABLE t_libuse_gregorova_project_SQL_secondary_final AS
-SELECT ec.country, ec.YEAR, ec.population, ec.GDP, ec.gini,
-		cp.value AS payroll, avg(cp2.value) AS price
+SELECT ec.country, ec.YEAR, ec.population, ec.GDP, ec.gini, cp.value AS payroll, cp2.value
 FROM europe_countries ec 
 JOIN countries c 
     ON ec.country = c.country
     AND region_in_world LIKE '%Europe%'
-JOIN 
+LEFT JOIN 
 	czechia_payroll cp 
 ON ec.YEAR = cp.payroll_year AND
     cp.value_type_code = 5958 AND 
-    calculation_code = 200   
-JOIN czechia_price cp2 
-ON ec.YEAR = YEAR(cp2.date_from) AND 
-	cp2.region_code IS NULL 
-;
+    calculation_code = 200 AND 
+    c.country = 'Czech Republic'
+LEFT JOIN czechia_price cp2 
+ON ec.YEAR = YEAR(cp2.date_from) 
+	AND cp2.region_code IS NULL
+	AND ec.country = 'Czech Republic';
