027294071cb705c7cef594c78b49a2ab962df800
LibuseG <libusegregorova@seznam.cz>
2024-01-06 17:52:00 +0100

    final commit

--- Projekt_4.sql
+++ Projekt_4.sql
@@ -16,11 +16,11 @@ WITH milk_and_bread AS (
     FROM czechia_price
     WHERE category_code = 111301 OR category_code = 114201
 )
-SELECT a.food_category , a.payroll_year, round(avg(a.avg_payroll)) AS avg_yearly_payroll, round(avg(a.price),2) AS avg_yearly_price,
-	round(a.avg_payroll/a.price) AS amount_to_buy, a.price_unit
+SELECT a.food_category ,a.payroll_year, round(avg(a.avg_payroll)) AS avg_yearly_payroll, round(avg(a.price),2) AS avg_yearly_price,
+	round(avg(a.avg_payroll)/avg(a.price)) AS amount_to_buy, a.price_unit
 FROM milk_and_bread 
 left JOIN t_libuse_gregorova_project_sql_primary_final AS a
 ON milk_and_bread.category_code = a.category_code 
- AND a.payroll_year IN (2006, 2018)
+AND a.payroll_year IN (2006, 2018)
 group BY a.payroll_year, a.food_category
 ;
