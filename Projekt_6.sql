28b1a7c061b4e55d146e5769cc9b2cfcedbd3fbe
LibuseG <libusegregorova@seznam.cz>
2024-01-06 18:32:36 +0100

    Revert "final commit"
    This reverts commit 548c805234b0896d13612b6894d3fde4d096634d.

--- Projekt_6.sql
+++ Projekt_6.sql
@@ -3,16 +3,16 @@
  výrazně vyšší než růst mezd (větší než 10 %)?--
  
  
- CREATE OR REPLACE temporary TABLE payroll_price_change AS (
- SELECT payroll_year, avg(avg_payroll) as avg_payroll, avg(price) as avg_price
- FROM t_libuse_gregorova_project_sql_primary_final tlgpspf 
- group BY payroll_year);
+ CREATE OR REPLACE TABLE payroll_price_change AS (
+ SELECt payroll_year, avg_payroll, avg(price) as avg_price
+        FROM t_libuse_gregorova_project_sql_primary_final tlgpspf 
+       group BY payroll_year);
  
 select 
-	a.payroll_year, round(a.avg_payroll), round(((b.avg_payroll-a.avg_payroll)/a.avg_payroll),3) as payroll_chng,
-	round(a.avg_price,2)as avg_price, round(((b.avg_price - a.avg_price)/a.avg_price),3) as price_chng,
+	a.payroll_year, a.avg_payroll, round(((b.avg_payroll-a.avg_payroll)/a.avg_payroll),3) as payroll_chng,
+	a.avg_price, round(((b.avg_price - a.avg_price)/a.avg_price),3) as price_chng,
 	CASE
- 		WHEN ((b.avg_price - a.avg_price)/a.avg_price) - ((b.avg_payroll-a.avg_payroll)/a.avg_payroll)> 0.01 THEN 1
+ 		WHEN ((b.avg_price - a.avg_price)/a.avg_price) - ((b.avg_payroll-a.avg_payroll)/a.avg_payroll)> 0.1 THEN 1
  		ELSE 0
  	END AS more_than_10
  FROM payroll_price_change a
@@ -20,3 +20,11 @@ select
  on a.payroll_year = b.payroll_year-1
  ;
 
+
+	(b.avg_payroll-a.avg_payroll)/a.avg_payroll AS pct_payroll_change,
+ 	(avg(b.price) - avg(a.price))/avg(a.price) AS pct_price_change
+ 	CASE
+ 		WHEN (pct_price_change)-(pct_payroll_change)> 0,1 THEN 1
+ 		ELSE 0
+ 	END 
+ 	
