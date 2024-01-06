6a3f22fe6501897fc28d57a619b5c54f9318ad7e
LibuseG <libusegregorova@seznam.cz>
2024-01-06 15:36:25 +0100

    commit

--- Projekt_6.sql
+++ Projekt_6.sql
@@ -3,15 +3,22 @@
  výrazně vyšší než růst mezd (větší než 10 %)?--
  
  
-SELECT
- 	a.payroll_year,
- 	b.payroll_year AS previous_year
-FROM t_libuse_gregorova_project_SQL_primary_final a
-left JOIN t_libuse_gregorova_project_SQL_primary_final b
-	ON a.payroll_year = b.payroll_year-1
-	AND a.food_category = b.food_category  
-GROUP BY a.payroll_year;
-
+ CREATE OR REPLACE TABLE payroll_price_change AS (
+ SELECt payroll_year, avg_payroll, avg(price) as avg_price
+        FROM t_libuse_gregorova_project_sql_primary_final tlgpspf 
+       group BY payroll_year);
+ 
+select 
+	a.payroll_year, a.avg_payroll, round(((b.avg_payroll-a.avg_payroll)/a.avg_payroll),3) as payroll_chng,
+	a.avg_price, round(((b.avg_price - a.avg_price)/a.avg_price),3) as price_chng,
+	CASE
+ 		WHEN ((b.avg_price - a.avg_price)/a.avg_price) - ((b.avg_payroll-a.avg_payroll)/a.avg_payroll)> 0.1 THEN 1
+ 		ELSE 0
+ 	END AS more_than_10
+ FROM payroll_price_change a
+ join payroll_price_change b
+ on a.payroll_year = b.payroll_year-1
+ ;
 
 
 	(b.avg_payroll-a.avg_payroll)/a.avg_payroll AS pct_payroll_change,
