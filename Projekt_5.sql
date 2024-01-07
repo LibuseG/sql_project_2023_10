1ba25af232a16a9538b76a5f131a906ee6f4788c
LibuseG <libusegregorova@seznam.cz>
2024-01-07 12:30:33 +0100

    correction of calculation

--- Projekt_5.sql
+++ Projekt_5.sql
@@ -4,14 +4,19 @@
 
 create or replace temporary table food_cathegory_increase as (
 SELECT food_category, payroll_year,
-	   price
+	   avg(price) as avg_price
 fROM t_libuse_gregorova_project_sql_primary_final 
 	group BY food_category, payroll_year
 );
 
-SELECT a.food_category, a.payroll_year, avg(a.price),avg(b.price),
-	   (avg(b.price) - avg(a.price))/avg(a.price) as year_chng
+SELECT a.food_category, a.payroll_year,b.payroll_year,c.payroll_year, a.avg_price, c.avg_price,
+	   (b.avg_price - a.avg_price)/a.avg_price as year_chng
 FROM food_cathegory_increase a
 left JOIN food_cathegory_increase b
 ON a.payroll_year = b.payroll_year-1 
-group BY a.payroll_year, a.food_category;
+and a.food_category = b.food_category
+left JOIN food_cathegory_increase c
+ON b.payroll_year = c.payroll_year
+and a.food_category = c.food_category
+group BY a.food_category, a.payroll_year
+;
