cf6ffd3b85ab15aa36c92940ec9e98fb63dcf75f
LibuseG <libusegregorova@seznam.cz>
2024-01-07 10:50:31 +0100

    add difference

--- Projekt_5.sql
+++ Projekt_5.sql
@@ -2,36 +2,16 @@
 --3. Která kategorie potravin zdražuje nejpomaleji 
 (je u ní nejnižší percentuální meziroční nárůst)?--
 
-
-WITH first_year AS (
-	SELECT food_category,
-		payroll_year,
-		price
-	FROM t_libuse_gregorova_project_sql_primary_final 
-	ORDER BY payroll_year
-	LIMIT 1),
-	last_year AS (
-	SELECT food_category,
-		payroll_year,
-		price
-	FROM t_libuse_gregorova_project_sql_primary_final 
-	ORDER BY payroll_year DESC 
-	LIMIT 1),
-food_category_increase AS (
-	SELECT 
-		food_category,
-		payroll_year,
-		price
-	FROM t_libuse_gregorova_project_SQL_primary_final
+with food_cathegory_increase as (
+SELECT food_category, payroll_year,
+	   price
+fROM t_libuse_gregorova_project_sql_primary_final 
+	group BY food_category, payroll_year
 )
-SELECT a.food_category,
-	a.payroll_year,
-	a.price,
-	fci.payroll_year AS prev_year,
-	(fci.price - a.price)/a.price AS price_change
-FROM t_libuse_gregorova_project_SQL_primary_final a
-JOIN food_category_increase fci
- ON a.payroll_year=fci.payroll_year-1 
- AND a.food_category = fci.food_category
- GROUP BY a.food_category
-;
+SELECT a.food_category, a.payroll_year, avg(a.price),avg(b.price),
+	   (avg(b.price) - avg(a.price))/avg(a.price)
+FROM food_cathegory_increase a
+left JOIN food_cathegory_increase b
+ON a.food_category = b.food_category 
+AND a.payroll_year = b.payroll_year-1 
+group BY a.payroll_year;
